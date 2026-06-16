756f434b33b8669e9091ff2510a0ffc726508292
contrerasrdianac406 <contrerasrdianac@gmail.com>
2026-06-15 19:20:45 -0500

    Feat[authors.controllers.js]:se crean los controller con la configuracion a la BD  para los endpoints de los autores

--- src/controllers/authors.controllers.js
+++ src/controllers/authors.controllers.js
@@ -1,134 +1,169 @@
-const { authors } = require("../data/authors");
+// conexión de la data creada localmente
+//const { authors } = require("../data/authors");
 
-// POST/authors
-const crearAutores = (req, res) => {
-  //Extraer datos del cuerpo de la solicitud
-  const { name, email, bio, created_at } = req.body;
 
-  //Validar que los campos requeridos estén presentes
-  if (!name || !email) {
-    // Si falta name o author, responder con error 400 (Bad Request)
-    return res
-      .status(400)
-      .json({ error: "El nombre y el correo electrónico son obligatorios" });
+// pool para la conexion de la BD
+const { Pool } = require("pg");
+const pool = require("../db/config");
+const { response } = require("express");
+
+// POST/authors - creación de autores
+const crearAutores = async (req, res) => {
+  try {
+    //Extraer datos del cuerpo de la solicitud
+    const { name, email, bio } = req.body;
+
+    //Validar que los campos requeridos estén presentes
+    if (!name || !email) {
+      // Si falta name o author, responder con error 400 (Bad Request)
+      return res
+        .status(400)
+        .json({ error: "El nombre y el correo electrónico son obligatorios" });
+    }
+
+    //Validar que el correo electrónico no esté repetido
+    const emailExists = await pool.query(
+      "SELECT * FROM authors WHERE email = $1",
+      [email],
+    );
+    if (emailExists.rows.length > 0) {
+      return res
+        .status(409)
+        .json({ error: "El correo electrónico ya está registrado" });
+    }
+
+    const result = await pool.query(
+      "INSERT INTO authors (name, email, bio) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, bio],
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error("Error al obtener el autor:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
   }
-
-  //Validar que el correo electrónico no esté repetido
-  const emailExists = authors.some((author) => author.email === email);
-  if (emailExists) {
-    // Si el correo electrónico ya existe, responder con error 400 (Bad Request)
-    return res
-      .status(400)
-      .json({ error: "El correo electrónico ya está registrado" });
-  }
-
-  // Crear el nuevo objeto de autor
-  const newAuthor = {
-    id: authors.length + 1, // Asignar un ID único autoincremental,
-    name, // Equivalente a: name: nombre del autor
-    email, // Equivalente a: email: correo electrónico del autor
-    bio, // Equivalente a: bio: biografía del autor
-    created_at: created_at || new Date().toISOString(), // Equivalente a: created_at: fecha de creación del autor
-  };
-
-  // GUARDAR el autor en nuestro "database" (array)
-  authors.push(newAuthor);
-
-  //RESPONDER con 201 Created (NO 200!)
-  //201 indica que se creo in recurso de manera exitosa
-  res.status(201).json(newAuthor);
 };
 
-//GET/authors
-const obtenerTodosAutores = (req, res) => {
-  res.status(200).json(authors);
+//GET/authors : obtener todos los autores
+const obtenerTodosAutores = async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM authors");
+    res.status(200).json(result.rows);
+    //Esta linea de codigo me permite hacer la consulta de desde la data local
+    //res.status(200).json(authors);
+  } catch (error) {
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
 };
 
-//GET/authors/:id
-const obtenerUnAutor = (req, res) => {
-  // EXTRAER el ID de los parámetros de la URL
-  const id = Number(req.params.id); // Convertir el ID a número
+//GET/authors/:id : obtener autor por id
+const obtenerUnAutor = async (req, res) => {
+  try {
+    // EXTRAER el ID de los parámetros de la URL
+    const id = Number(req.params.id); // Convertir el ID a número
 
-  //VALIDAR que el ID sea un número válido
-  if (Number.isNaN(id)) {
-    return res.status(400).json({ error: "El ID debe ser un número válido" });
-  }
-  //BUSCAR el autor en el array
-  const author = authors.find((author) => author.id === id);
+    //VALIDAR que el ID sea un número válido
+    if (Number.isNaN(id)) {
+      return res.status(400).json({ error: "El ID debe ser un número válido" });
+    }
+    //validar que el Id sea positivo
+    if (id <= 0) {
+      return res
+        .status(400)
+        .json({ error: "El ID debe ser un número positivo" });
+    }
 
-  //VALIDAR si el autor existe
-  if (!author) {
-    return res.status(404).json({ error: "Autor no encontrado" });
+    // consultar un autor en la base de datos
+    const result = await pool.query("SELECT * FROM authors WHERE id = $1", [id]);
+
+    const authors = result.rows;
+
+    //BUSCAR el autor en el array
+    const author = authors.find((author) => author.id === id);
+
+    //VALIDAR si el autor existe
+    if (!author) {
+      return res.status(404).json({ error: "Autor no encontrado" });
+    }
+    //RESPONDER con el autor encontrado
+    res.status(200).json(author);
+  } catch (error) {
+    console.error("Error al obtener el autor:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
   }
-  //RESPONDER con el autor encontrado
-  res.status(200).json(author);
 };
 
-//PUT/authors/:id
-const actualizarUnAutor = (req, res) => {
-  //EXTRAER y VALIDAR el ID
-  const id = Number(req.params.id);
-  // Validar que el ID sea un número válido
-  if (Number.isNaN(id)) {
-    return res.status(400).json({ error: "El ID debe ser un número válido" });
+//PUT/authors/:id : actualizar un autor
+const actualizarUnAutor = async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    // Validar que el ID sea un número válido
+    if (Number.isNaN(id)) {
+      return res.status(400).json({ error: "El ID debe ser un número válido" });
+    }
+
+    //Extraer los datos del body de la solicitud
+    const { name, email, bio, created_at } = req.body;
+    //EXTRAER y VALIDAR el ID
+
+    // VALIDAR que los datos requeridos estén presentes
+    if (!name || !email) {
+      return res
+        .status(400)
+        .json({ error: "El nombre y el correo electrónico son obligatorios" });
+    }
+
+    // Validar si el autor existe
+    const autorExiste = await pool.query(
+      "SELECT * FROM authors WHERE id = $1",
+      [id],
+    );
+
+    //BUSCAR el autor
+    if (autorExiste.rows.length === 0) {
+      return res.status(404).json({ error: "Autor no encontrado" });
+    }
+    const result = await pool.query(
+      "UPDATE authors SET name = $1, email = $2, bio = $3 WHERE id = $4 RETURNING *",
+      [name, email, bio, id],
+    );
+
+    // Responder con el autor actualizado
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.error("Error al obtener el autor:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
   }
-
-  //Extraer los datos del body de la solicitud
-  const { name, email, bio, created_at } = req.body;
-
-  // VALIDAR que los datos requeridos estén presentes
-  if (!name || !email) {
-    return res
-      .status(400)
-      .json({ error: "El nombre y el correo electrónico son obligatorios" });
-  }
-
-  //BUSCAR el índice del libro en el array
-  const authorIndex = authors.findIndex((author) => author.id === id);
-
-  // Validar si el autor existe
-  if (authorIndex === -1) {
-    return res.status(404).json({ error: "Autor no encontrado" });
-  }
-
-  // REMPLAZAR el autor completo en esta posicion
-  authors[authorIndex] = {
-    id, // mantener el mismo ID
-    name,
-    email,
-    bio,
-    created_at: created_at || new Date().toISOString(),
-  };
-
-  // Responder con el autor actualizado
-  res.status(200).json(authors[authorIndex]);
 };
 
-//DELETE/authors/:id
-const eliminarUnAutor = (req, res) => {
+//DELETE/authors/:id : eliminar un autor
+const eliminarUnAutor = async (req, res) => {
+  try {
   // extraer y validar el ID
-
   const id = Number(req.params.id);
 
   if (Number.isNaN(id)) {
     return res.status(400).json({ error: "El id debe ser un número " });
   }
 
-  // Buscar el indice de un autor
-  const index = authors.findIndex((author) => author.id === id);
+  //Verificar si el autor existe
 
-  //Validar que el autor exista
-  if (index === -1) {
+  const autorExiste = await pool.query("SELECT * FROM authors WHERE id = $1",[id],);
+  
+  if (autorExiste.rows.length === 0) {
     return res.status(404).json({ error: "Autor no encontrado" });
   }
 
-  //Eliminar el autor del arrayy guardar referencia
+  //Eliminar autor por ID
+  const result = await pool.query("DELETE FROM authors WHERE id = $1 RETURNING *", [id]);
 
-  const deleted = authors.splice(index, 1)[0];
+  //Responder con el autor eliminado
+  res.status(200).json(result.rows[0]);
 
-  // Responder con el autor eliminado
-
-  res.status(200).json(deleted);
+  } catch (error){
+    console.error("Error al obtener el autor:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+}
 };
 
 module.exports = {
